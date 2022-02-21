4411f099b933f70a2fe8484230fcd938c2212269
kubagrzelak <jakubgrzelak1@gmail.com>
2022-02-21 12:26:48 +0000

    Working POC

--- image/src/app/config/application.py
+++ image/src/app/config/application.py
@@ -7,7 +7,7 @@ from app.utils.intenv import intenv
 
 APPLICATION = {
     "SERVER_HOST_ADDRESS": env("SERVER_HOST_ADDRESS", "127.0.0.1"),
-    "SERVER_HOST_PORT": env("SERVER_HOST_PORT", "12345"),
+    "SERVER_HOST_PORT": intenv("SERVER_HOST_PORT", 12345),
     "FUNCTION": env("FUNCTION", "holding_registers"),
     "START_ADDRESS": intenv("START_ADDRESS", 0),
     "LENGTH": intenv("LENGTH", 1),
